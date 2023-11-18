aacce346225e14323f504a616d3befb288dd301f
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-18 13:30:24 +0900

    init commit

---
+++ src/test/java/com/lv5/lv5/Lv5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv5.lv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
