c752901108116d68f11f461f55936b9d063950ce
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-18 19:22:25 +0900

    회원가입 메서드가 ResponseEntity를 반환하도록 수정

--- src/main/java/com/lv5/lv5/controller/UserController.java
+++ src/main/java/com/lv5/lv5/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.lv5.lv5.controller;
 
-import java.util.List;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,19 +22,15 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/user/signup")
-	public String signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
-		// Validation 예외처리
-		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-		if (fieldErrors.size() > 0) {
-			for (FieldError fieldError : bindingResult.getFieldErrors()) {
-				log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-			}
-			return "Validation엣 ㅓ걸림";
+	public ResponseEntity<String> signup(@Valid @RequestBody SignupRequestDto requestDto) {
+		try {
+			userService.signup(requestDto);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest()
+				.body(e.toString());
 		}
-
-		userService.signup(requestDto);
-
-		return "회원가입 됨";
+		return ResponseEntity.ok()
+			.body("회원가입 되었습니다.");
 	}
 
 }
